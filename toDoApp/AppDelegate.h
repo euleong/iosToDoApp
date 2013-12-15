c3663dea2c35322878fdc8900f5a2ac6ee921f45
euleong <el@eugenialeong.com>
2013-12-15 15:34:25 -0800

    Initial Commit

---
+++ toDoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  toDoApp
+//
+//  Created by Eugenia Leong on 12/15/13.
+//  Copyright (c) 2013 Eugenia Leong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ toDoAppTests/toDoAppTests.m
@@ -0,0 +1,34 @@
+//
+//  toDoAppTests.m
+//  toDoAppTests
+//
+//  Created by Eugenia Leong on 12/15/13.
+//  Copyright (c) 2013 Eugenia Leong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface toDoAppTests : XCTestCase
+
+@end
+
+@implementation toDoAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
